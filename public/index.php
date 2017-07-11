9355d4b7699c518167b3c1ab1680e6712f5d4cc0
Ruslan Kladko <rkladko@gmail.com>
2017-07-11 10:37:00 +0300

    Boilerplate

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types = 1);
+/**
+ * Created by PhpStorm.
+ * User: russ
+ * Date: 11.07.17
+ * Time: 10:11
+ */
+
+require __DIR__ . '/../src/Bootstrap.php';
---
+++ src/Bootstrap.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types = 1);
+/**
+ * Created by PhpStorm.
+ * User: russ
+ * Date: 11.07.17
+ * Time: 10:12
+ */
+
+echo 'I need job shortly!';
